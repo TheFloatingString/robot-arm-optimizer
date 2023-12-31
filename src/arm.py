8d8fcd7595eca22256cc54adff90dc3632b1bcc6
Laurence Liang <laurence.w.liang@gmail.com>
2023-12-30 23:58:42 -0500

    Add placeholder files

---
+++ src/arm.py
@@ -0,0 +1,12 @@
+"""
+This file contains the class for the robot arm
+"""
+
+import numpy as np
+
+class Arm2Link:
+    def __init__(self):
+        pass
+
+    def get_position(self, theta_vector):
+        return None
